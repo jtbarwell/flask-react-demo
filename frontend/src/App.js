0b1fd4a769e8c6b4bc5497df440179ad43913cea
Joshua Barwell <jtbarwell@gmail.com>
2025-07-28 15:21:34 -0400

    changed indenting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,22 +11,22 @@ function App() {
     }, []);
 
     return (
-    <table id="People_Table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
+        <table id="People_Table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(item => (
+                <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
             </tr>
-        </thead>
-        <tbody>
-            {data.map(item => (
-            <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-        </tr>
-            ))}
-    </tbody>
-    </table>
+                ))}
+        </tbody>
+        </table>
     );
 }
 
