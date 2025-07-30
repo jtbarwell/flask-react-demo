7c670f1a31a03db58468a0a4aafb97c8ca37eb04
Joshua Barwell <jtbarwell@gmail.com>
2025-07-30 16:28:30 -0400

    moved BattingPageXXX to its own folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './globalCSS/Table.css';
 
 import HomePage from './HomePageFolder/HomePage';
 import MyRosterPage from './MyRosterPageFolder/MyRosterPage';
-import BattingPage from './BattingPage';
+import BattingPage from './BattingPageFolder/BattingPage';
 import PitchingPage from './PitchingPage';
 import FieldingPage from './FieldingPage';
 
