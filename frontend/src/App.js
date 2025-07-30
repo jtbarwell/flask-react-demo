c3aefa8d00f8c737898dfa2f868573b5b2d91c71
Joshua Barwell <jtbarwell@gmail.com>
2025-07-30 16:23:37 -0400

    added CSS files to \src\globalCSS and maintained the references

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './Table.css';
+import './globalCSS/Table.css';
 
 import HomePage from './HomePage';
 import MyRosterPage from './MyRosterPage';
