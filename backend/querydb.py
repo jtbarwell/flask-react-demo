a6ef8537fe440b929a34596c152d0c5486fbff63
Joshua Barwell <jtbarwell@gmail.com>
2025-08-11 16:47:51 -0400

    updated the database connection functions

--- backend/querydb.py
+++ backend/querydb.py
@@ -4,17 +4,29 @@ import cloudscraper
 from bs4 import BeautifulSoup
 import time
 
-# server = 'your_server_name'
+# server = 'DESKTOP-IEKNRR8\SQLEXPRESS'
 # database = 'baseball'
-# username = 'your_username'
-# password = 'your_password'
-# driver = '{ODBC Driver 18 for SQL Server}'
+# driver = '{ODBC Driver 17 for SQL Server}'
 
 
-def query_to_JSON(server, db, usr, pswd, driver, query):
-    connection_string = f'DRIVER={driver};SERVER={server};DATABASE={db};UID={usr};PWD={pswd}'
+def query_to_JSON(server, db, driver, query):
+    connection_string = f'Driver={driver};Server={server};Database={db};Trusted_Connection=yes;'
     with pyodbc.connect(connection_string) as conn:
         resultDF = pd.read_sql(query, conn)
         jsonResult = resultDF.to_json()
     
     return jsonResult
+
+
+def insert_query(server, db, driver, query, params=None):
+    connection_string = f'DRIVER={driver};SERVER={server};DATABASE={db};Trusted_Connection=yes;'
+    with pyodbc.connect(connection_string) as conn:
+        cursor = conn.cursor()
+        if params:
+            cursor.execute(query, params)
+        else:
+            cursor.execute(query)
+        conn.commit()
+
+        
+
