3a520f5670c6945035a8834d6a57993e77e47d14
Joshua Barwell <jtbarwell@gmail.com>
2025-08-13 10:31:06 -0400

    Added a years drop-down menu and passed the year as a condition to battingtable.js

--- frontend/src/BattingPageFolder/AllStatsBattingPage.js
+++ frontend/src/BattingPageFolder/AllStatsBattingPage.js
@@ -2,7 +2,20 @@ import React, { useEffect, useState } from 'react';
 import BattingTable from './BattingTable';
 
 export default function AllStatsBattingPage() {
+  const currentYear = new Date().getFullYear();
+  const startYear = 1990;
+  const [selectedYear, setSelectedYear] = useState(currentYear);
   const [batting, setBatting] = useState([]);
+
+
+//   const condition = selectedYear === "all" ? (row) => row.Year >= 0 : (row) => row.Year === Number(selectedYear);
+
+  const condition = React.useCallback(
+    selectedYear === "all"
+        ? true
+        : (row) => Number(row.Year) === Number(selectedYear),
+    [selectedYear]
+  );
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/api/batting')
@@ -15,7 +28,15 @@ export default function AllStatsBattingPage() {
     <div>
       <h1>Batting Page</h1>
       <p>This is where the batting stats and lists will go!</p>
-      <BattingTable data={batting} />
+      <select id="years" value={selectedYear} onChange={(e) => setSelectedYear(Number(e.target.value))}>
+        <option key="all" value="all">All Years</option>
+        {Array.from({ length: currentYear - startYear + 1 }, (_, i) => currentYear - i).map((year) => (
+            <option key={year} value={year}>
+            {year}
+            </option>
+        ))}
+      </select>
+      <BattingTable data={batting} condition={condition} />
     </div>
   );
 }
--- frontend/src/BattingPageFolder/LeaderboardBattingPage.js
+++ frontend/src/BattingPageFolder/LeaderboardBattingPage.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import BattingTable from './BattingTable';
 
 export default function AllStatsBattingPage() {
+  const currentYear = new Date().getFullYear();
+  const startYear = 1990;
+  const [selectedYear, setSelectedYear] = useState(currentYear);
   const [batting, setBatting] = useState([]);
 
   useEffect(() => {
@@ -11,11 +14,32 @@ export default function AllStatsBattingPage() {
       .catch(err => console.error(err));
   }, []);
 
+
+  
+  const condition = React.useCallback(
+    selectedYear === "all"
+        ? true
+        : (row) => Number(row.Year) === Number(selectedYear),
+    [selectedYear]
+  );
+
+  const leaderCondition = React.useCallback(
+    (row) => row.AB >= 100
+  );
+
   return (
     <div>
       <h1>Batting Leaders</h1>
       <p>Leader is determined by whether their OPS is &gt;= 0.850</p>
-      <BattingTable data={batting} condition={row => row.OPS >= 0.850} />
+      <select id="years" value={selectedYear} onChange={(e) => setSelectedYear(Number(e.target.value))}>
+        <option key="all" value="all">All Years</option>
+        {Array.from({ length: currentYear - startYear + 1 }, (_, i) => currentYear - i).map((year) => (
+            <option key={year} value={year}>
+            {year}
+            </option>
+        ))}
+      </select>
+      <BattingTable data={batting} condition={(row) => row.OPS >= 0.850 && leaderCondition(row) && condition(row)} leaders={true}/>
     </div>
   );
 }
